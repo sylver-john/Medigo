b052731e7fc8104516b6df3d4f8ed2f8f8774b61
Sylver Imhoff <Sylver-John.IMHOFF.ext@boursorama.fr>
2017-02-22 15:46:29 +0100

    insert to mongoDb

--- src/handler/handler.go
+++ src/handler/handler.go
@@ -9,10 +9,11 @@ import (
 
 func Index(c *gin.Context) {
 	inventory := service.GetInventory()
-	drugs := service.GetDrugs(inventory[0:20])
+	drugs := service.GetDrugs(inventory[0:200])
 	for _, value := range drugs {
-		if value != nil {
+		if value != nil && 0 < len(value) {
 			log.Println(value)
+			service.StoreDrug(value)
 		}
 	}
   	c.File("C:/Users/simhoff/Medigo/front/index.html")
--- src/service/db.go
+++ src/service/db.go
@@ -1,1 +1,27 @@
 package service
+
+import(
+  "log"
+  m "../model"
+  "gopkg.in/mgo.v2"
+)
+
+func connectToDb() *mgo.Session {
+  session, err := mgo.Dial("**********")
+  if err != nil {
+    log.Fatal(err)
+  }
+  return session
+}
+
+func StoreDrug(drugs []m.Drug) {
+  session := connectToDb()
+  defer session.Close()
+  collection := session.DB("medigo").C("drugs")
+  for i := 0; i < len(drugs); i++ {
+    err := collection.Insert(drugs[i])
+    if err != nil {
+      log.Println(err)
+    }
+  }
+}
